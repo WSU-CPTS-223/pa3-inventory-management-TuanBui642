35df5b7faa2ab3c33e28c0827d43bd54e2dd731d
Tuan Bui <tuanbui21700@gmail.com>
2025-10-25 17:28:00 -0700

    Added Seperate Files for starter code, tweaked and set up debugger compiler stuff

---
+++ MainHeader.hpp
@@ -0,0 +1,21 @@
+/*
+    Name: Tuan Bui
+    Date: 10/25/25
+    Program: PA3, Inventory Management
+*/
+
+#ifndef MAINHEADER
+#define MAINHEADER
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void printHelp();
+bool validCommand(string line);
+void evalCommand(string line);
+void bootStrap();
+
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,54 +1,10 @@
-#include <iostream>
-#include <string>
+/*
+    Name: Tuan Bui
+    Date: 10/25/25
+    Program: PA3, Inventory Management
+*/
 
-using namespace std;
-
-void printHelp()
-{
-    cout << "Supported list of commands: " << endl;
-    cout << " 1. find <inventoryid> - Finds if the inventory exists. If exists, prints details. If not, prints 'Inventory not found'." << endl;
-    cout << " 2. listInventory <category_string> - Lists just the id and name of all inventory belonging to the specified category. If the category doesn't exists, prints 'Invalid Category'.\n"
-         << endl;
-    cout << " Use :quit to quit the REPL" << endl;
-}
-
-bool validCommand(string line)
-{
-    return (line == ":help") ||
-           (line.rfind("find", 0) == 0) ||
-           (line.rfind("listInventory") == 0);
-}
-
-void evalCommand(string line)
-{
-    if (line == ":help")
-    {
-        printHelp();
-    }
-    // if line starts with find
-    else if (line.rfind("find", 0) == 0)
-    {
-        // Look up the appropriate datastructure to find if the inventory exist
-        cout << "YET TO IMPLEMENT!" << endl;
-    }
-    // if line starts with listInventory
-    else if (line.rfind("listInventory") == 0)
-    {
-        // Look up the appropriate datastructure to find all inventory belonging to a specific category
-        cout << "YET TO IMPLEMENT!" << endl;
-    }
-}
-
-void bootStrap()
-{
-    cout << "\n Welcome to Amazon Inventory Query System" << endl;
-    cout << " enter :quit to exit. or :help to list supported commands." << endl;
-    cout << "\n> ";
-    // TODO: Do all your bootstrap operations here
-    // example: reading from CSV and initializing the data structures
-    // Don't dump all code into this single function
-    // use proper programming practices
-}
+#include "MainHeader.hpp"
 
 int main(int argc, char const *argv[])
 {
@@ -68,3 +24,13 @@ int main(int argc, char const *argv[])
     }
     return 0;
 }
+
+
+/*
+
+Uniq Id,Product Name,Brand Name,Asin,Category,Upc Ean Code,List Price,Selling 
+    Price,Quantity,Model Number,About Product,Product Specification,Technical Details,
+        Shipping Weight,Product Dimensions,Image,Variants,Sku,Product Url,Stock,Product Details,Dimensions,
+            Color,Ingredients,Direction To Use,Is Amazon Seller,Size Quantity Variant,Product Description
+
+*/
